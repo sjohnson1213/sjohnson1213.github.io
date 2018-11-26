eb029e92e79a681ac3b2dbc7de9ba16acea1a5c0
Serene Matheson <joh13085@byui.edu>
2018-11-26 11:57:02 -0800

    updating page js

--- assignments/weathersite/js/preston.js
+++ assignments/weathersite/js/preston.js
@@ -6,12 +6,12 @@ weatherRequest.onload=function(){
     var weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
     
-    document.getElementById('pres_current').innerHTML = weatherData.weather[0].main;
-    document.getElementById('pres_temp_current').innerHTML = weatherData.main.temp;
-    document.getElementById('pres_humid').innerHTML = weatherData.main.humidity;
-    document.getElementById('pres_precip').innerHTML = weatherData.clouds.all;
-    document.getElementById('pres_wind_speed').innerHTML = weatherData.wind.speed;
-    document.getElementById('pres_currentword').innerHTML = weatherData.weather[0].main;
+    document.getElementById('current').innerHTML = weatherData.weather[0].main;
+    document.getElementById('temp_current').innerHTML = weatherData.main.temp;
+    document.getElementById('humid').innerHTML = weatherData.main.humidity;
+    document.getElementById('precip').innerHTML = weatherData.clouds.all;
+    document.getElementById('wind_speed').innerHTML = weatherData.wind.speed;
+    document.getElementById('currentword').innerHTML = weatherData.weather[0].main;
 
     var iconcode = weatherData.weather[0].icon;
     var icon_path = "http://openweathermap.org/img/w/" + iconcode + ".png";
